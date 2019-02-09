2def7174a3afba89df0b7dc4de96849f962a8cb4
k9rosie <k9rosie@gmail.com>
2019-02-09 02:55:56 -0600

    ok we get a proper schema retrieve working now

---
+++ src/components/presentational/Carousel.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Carousel extends Component {
+    render() {
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import state from './reducers';
 import Root from './Root';
+import Swagger from 'swagger-client';
 
 const store = createStore(state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+const opts = {
+    headers: 'application/vnd.oai.openapi+json'
+};
+
+Swagger({url: "http://localhost:8000/schema/",
+    requestInterceptor: (req) => {
+        req.headers.Accept = opts.headers;
+    }
+}).then(client => {
+    ReactDOM.render(<Root client={client} store={store}/>, document.getElementById('root'));
+    console.log(client);
+});
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
